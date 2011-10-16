05584eef58c933daeadfd2572e8bf0e6941398a9
Peter Beams <peter.beams@googlemail.com>
2011-10-16 17:14:38 +0100

    adding existing test files into user acceptance project

---
+++ ADLINQ/ADLINQ.Tests/SpecificationBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ADLINQ.Tests
+{
+    public class SpecificationBase
+    {
+        [SetUp]
+        public virtual void Given()
+        {
+        }
+
+        public virtual void When()
+        {
+        }
+    }
+}
